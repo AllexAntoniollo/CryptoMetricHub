d119e8573ad040c7363f861a1f8d372cae3863fa
Arthur Bogacki <abverissimo@inf.ufsm.br>
2024-01-04 01:43:34 -0300

    login_continua

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -6,7 +6,16 @@ export default function Login() {
     <div className="detail_radius"></div>{/* detail_radius*/}
     <div className="conteiner_login">
       <p className="conect_title">Connect your <h2>wallet</h2></p>
+      <div className="metamask-btn">
+        <div className="metamask_logo"></div>{/* matamask_Logo */}
+        <p className="metamask_title">Connect with MetaMask</p>  
+      </div>{/* metamask-btn */}
+      <div className="form">
+        <input type="text" placeholder="Address"></input>
+        <input type="submit"></input>
+      </div>{/* form */}
     </div> {/* conteiner_login */}
+    <div className="bg_btc"></div>
       
     </>
   );
