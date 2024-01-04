98c519f960726b3c7e18432e5dd995762e5ef889
Allex Antoniollo <antoniolloallex@gmail.com>
2024-01-04 07:11:49 -0300

    Create Assets dir

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,23 +1,29 @@
 import Header from "../components/header";
-import './login.css';
+import "./login.css";
 export default function Login() {
   return (
     <>
-    <div className="detail_radius"></div>{/* detail_radius*/}
-    <div className="conteiner_login">
-      <p className="conect_title">Connect your <h2>wallet</h2></p>
-      <div className="metamask-btn">
-        <div className="metamask_logo"></div>{/* matamask_Logo */}
-        <p className="metamask_title">Connect with MetaMask</p>  
-      </div>{/* metamask-btn */}
-      <div className="form">
-        <input type="text" placeholder="Address"></input>
-        <input type="submit"></input>
-      </div>{/* form */}
-    </div> {/* conteiner_login */}
-    <div className="bg_btc"></div>
-    <div className="bg_eth"></div>
-      
+      <div className="detail_radius"></div>
+      {/* detail_radius*/}
+      <div className="conteiner_login">
+        <p className="conect_title">
+          Connect your <h2>wallet</h2>
+        </p>
+        <div className="metamask-btn">
+          <div className="metamask_logo"></div>
+          {/* matamask_Logo */}
+          <p className="metamask_title">Connect with MetaMask</p>
+        </div>
+        {/* metamask-btn */}
+        <div className="form">
+          <input type="text" placeholder="Address"></input>
+          <input type="submit"></input>
+        </div>
+        {/* form */}
+      </div>{" "}
+      {/* conteiner_login */}
+      <div className="bg_btc"></div>
+      <div className="bg_eth"></div>
     </>
   );
 }
