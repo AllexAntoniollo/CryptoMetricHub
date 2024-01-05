e05b2833abd2307cd3c5c4efe6b39da48f6fba8e
Arthur Bogacki <abverissimo@inf.ufsm.br>
2024-01-05 10:31:36 -0300

    cotações_section

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -22,6 +22,30 @@ export default function Login() {
         {/* form */}
       </div>{" "}
       {/* conteiner_login */}
+      <div className="price_section">
+        <div className="price_content">
+          <div className="price_title">
+            <h1>PRICES</h1>  
+          </div>{/* price_title */}
+          <div className="price_table">
+            <div className="w33"><h1>CRIPTO</h1></div>  
+            <div className="w33"><h1>COTAÇÃO</h1></div>  
+            <div className="w33"><h1>24H</h1></div> 
+            <div className="w33 cripto"><h1>Btc</h1></div>  
+            <div className="w33 cripto"><h1>$40.000</h1></div>  
+            <div className="w33 cripto up"><h1>+2.35%</h1></div> 
+            <div className="w33 cripto"><h1>Eth</h1></div>  
+            <div className="w33 cripto"><h1>$1.000</h1></div>  
+            <div className="w33 cripto down"><h1>-1,02%</h1></div> 
+            <div className="w33 cripto"><h1>Matic</h1></div>  
+            <div className="w33 cripto"><h1>$0.88</h1></div>  
+            <div className="w33 cripto up"><h1>+4,54%</h1></div> 
+            <div className="w33 cripto"><h1>AlecRypto</h1></div>  
+            <div className="w33 cripto"><h1>$3x10^6</h1></div>  
+            <div className="w33 cripto up"><h1>+1000,23%</h1></div> 
+          </div>{/* price_table */}
+        </div>{/* price_content */}
+      </div>{/* price_section */}
       <div className="bg_btc"></div>
       <div className="bg_eth"></div>
     </>
